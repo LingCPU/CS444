7f8cc6c8d0ddccfe754559714dbb024f68a4158f
LingCPU <tommyle2489@gmail.com>
2025-05-03 15:35:14 -0700

    serial ports and navigation trees

--- rtStereo.cc
+++ rtStereo.cc
@@ -13,6 +13,65 @@
 using namespace cv;
 using namespace std;
 
+int serialPortOpen(){
+    int serial_port = open("/dev/ttyACM0", O_RDWR);
+    if(serial_port == -1){
+        perror("Error opening serial port");
+        return -1;
+    }
+
+    struct termios tty;
+    memset(&tty, 0, sizeof(tty));
+
+    if(tcgetattr(serial_port, &tty) != 0){
+        perror("Error getting current serial port settings");
+        return -1;
+    }
+
+    cfsetispeed(&tty, B9600);
+    cfsetospeed(&tty, B9600);
+
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= CREAD | CLOCAL;
+
+    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ECHO;
+    tty.c_lflag &= ~ECHOE;
+    tty.c_lflag &= ~ISIG;
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_iflag &= ~(ICRNL | INLCR);
+
+    tty.c_oflag &= ~OPOST;
+
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 5;
+
+    if(tcsetattr(serial_port, TCSANOW, &tty) != 0){
+        perror("Error setting serial port attributes");
+        return -1;
+    }
+
+    return serial_port;
+}
+
+int serialPortWrite(const char *data, int serial_port){
+    int n = write(serial_port, data, strlen(data));
+    if(n < 0){
+        perror("Error writing to serial port");
+        return -1;
+    }
+    return n;
+}
+
+int serialPortClose(int serial_port){
+    return close(serial_port);
+}
+
+
 void detectObstacles(const Mat& depthImage, bool& leftClear, bool& centerClear, bool& rightClear, 
                      double obstacleThreshold, Mat& obstacleImage){
                         
@@ -42,14 +101,11 @@ void detectObstacles(const Mat& depthImage, bool& leftClear, bool& centerClear, 
         for(int x = 0; x < cols; x++){
             // Get the depth value
             uchar depth = depthImage.at<uchar>(y, x);
-            
-            // If the pixel has a non-zero depth value and is closer than threshold
-            // The lower the depth value, the closer the object (we invert the check)
             if(depth > 0 && depth < obstacleThreshold){
                 // Mark as obstacle in the obstacle image
                 obstacleImage.at<uchar>(y, x) = 255;
                 
-                // Count obstacle in the appropriate region
+                // Count obstacle in the regions
                 if(x >= leftStart && x < leftEnd) leftObstacles++;
                 else if(x >= centerStart && x < centerEnd) centerObstacles++;
                 else if(x >= rightStart && x < rightEnd) rightObstacles++;
@@ -75,6 +131,18 @@ void detectObstacles(const Mat& depthImage, bool& leftClear, bool& centerClear, 
     line(obstacleImage, Point(0, horizonLine), Point(cols-1, horizonLine), Scalar(128), 2);   
 }
 
+void generateMotorCommand(bool leftClear, bool centerClear, bool rightClear, 
+                          char* command, int baseSpeed){
+    // Default is to stop if no clear path
+    strcpy(command, "STP000\n");
+    
+    // Decision tree for navigation
+    if(centerClear) sprintf(command, "FWD%03d\n", baseSpeed);
+    else if(leftClear && !rightClear) strcpy(command, "STR045\n");
+    else if(!leftClear && rightClear) strcpy(command, "STR135\n");
+    else if(leftClear && rightClear) strcpy(command, "STR045\n");
+}
+
 int main(int argc, char** argv){
 
     int fps = 60; // in frames per sec
@@ -83,6 +151,15 @@ int main(int argc, char** argv){
     int rows  = 480;
     int cols  = 640;
     Mat depthImage = Mat::zeros(rows,cols, CV_8UC1);
+    Mat obstacleImage = Mat::zeros(rows, cols, CV_8UC1);
+    double obstacleThreshold = 100;
+    int baseSpeed = 64;
+
+    // serial port things:
+    int serialPort = serialPortOpen();
+    if(serialPort < 0) cout << "Failed to open serial port";
+    else cout << "Opened serial port successfully";
+
 
     // Read rectification lookup tables
     Mat map1x,map1y,map2x,map2y;
@@ -121,6 +198,8 @@ int main(int argc, char** argv){
     cout << " height  \n" << capL.get(CAP_PROP_FRAME_HEIGHT)<<endl;
     cout << " format \n" << capL.get(CAP_PROP_FORMAT)<<endl;
     cout << " fps \n" << capL.get(CAP_PROP_FPS)<<endl;
+
+    if(serialPort >= 0) serialPortWrite("STP000\n", serialPort);
         
     while(true){
         // Capture frames from both cameras
@@ -143,12 +222,36 @@ int main(int argc, char** argv){
         Mat medianFiltered;
         medianBlur(depthImage, medianFiltered, 3);
 
+        // Detect obstacles and determine safe direction
+        bool leftClear, centerClear, rightClear;
+        detectObstacles(medianFiltered, leftClear, centerClear, rightClear, obstacleThreshold, obstacleImage);
+        
+        // Generate motor command based on obstacle detection
+        char command[8];
+        generateMotorCommand(leftClear, centerClear, rightClear, command, baseSpeed);
+        
+        // Send command to motor controller if serial port is open
+        if(serialPort >= 0) serialPortWrite(command, serialPort);
+
         // display depth map
         imshow("Depth",medianFiltered);
         hconcat(rectifiedLeft, rectifiedRight,both);
         imshow("Left and Right",both);
     
-        waitKey(frameDelay) ;
+        int key = waitKey(frameDelay);
+        if(key == 'q') break;
+        
+        // Emergency stop on spacebar
+        if(key == ' ' && serialPort >= 0) {
+            serialPortWrite("STP000\n", serialPort);
+            cout << "Emergency stop triggered!" << endl;
+        }
+    }
+
+    if(serialPort >= 0){
+        serialPortWrite("STP000\n", serialPort);
+        serialPortClose(serialPort);
+        cout << "Robot stopped and serial port closed." << endl;
     }
 
     // Release resources
