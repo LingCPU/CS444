0c6ad17eb01d7a5d26d0435e5a5c664eda2690ca
LingCPU <tommyle2489@gmail.com>
2025-05-09 12:16:45 -0700

    yes

--- stereoKernel.cu
+++ stereoKernel.cu
@@ -4,8 +4,8 @@
 #include <cuda_runtime.h>
 #include <limits.h>
 
-__global__ void stereoKernel(unsigned char* left, unsigned char* right, unsigned char* depth,
-                             double maxDistance, int rows, int cols){
+__global__ void stereoKernel(unsigned char* left, unsigned char* right,
+                             double maxDisparity, int rows, int cols){
 
     // compute the row and col of the pixel to be processed
     int col = blockIdx.x*blockDim.x + threadIdx.x;
